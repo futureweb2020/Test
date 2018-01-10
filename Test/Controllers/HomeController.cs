f0ca0aa208cd404d7a577928d3df861617fd6b89
futureweb2020 <futureweb2020@yandex.com>
2018-01-10 11:37:56 -0300

    completed

--- Test/Controllers/HomeController.cs
+++ Test/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Test.Models;
+using System.Text.RegularExpressions;
 
 namespace Test.Controllers
 {
@@ -15,23 +16,69 @@ namespace Test.Controllers
             return View();
         }
 
-        public IActionResult About()
+        [HttpPost]
+        public List<string> Check()
         {
-            ViewData["Message"] = "Your application description page.";
+            var Input = Request.Form["input"][0];
+            var Rows = Input.Split("\n");
+            Dictionary<string, string> Subjects = new Dictionary<string, string> { };
+            List<string> Courses = new List<string> { };
+            List<string> Temp = new List<string> { };
+            for(var i = 0; i < Rows.Length; i++)
+            {
+                var tmp = Rows[i].Split(": ");
+                if (tmp.Length > 1)
+                {
+                    Subjects.Add(tmp[0], tmp[1]);
+                    if(tmp[1]=="")
+                        Courses.Add(tmp[0]);
+                }
+                else
+                {
+                    Subjects.Add(tmp[0], null);
+                    Courses.Add(tmp[0]);
+                }
+            }
 
-            return View();
+            foreach (KeyValuePair<string, string> main in Subjects)
+            {
+                Temp = getList(Subjects, main, Temp);
+                if(Temp == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    Temp.Reverse();
+                    foreach (var i in Temp)
+                    {
+                        if (!Courses.Contains(i))
+                            Courses.Add(i);
+                    }
+                    if (!Courses.Contains(main.Key) && main.Key != null && main.Key != "")
+                        Courses.Add(main.Key);
+                    if (!Courses.Contains(main.Value) && main.Value != null && main.Value != "")
+                        Courses.Add(main.Value);
+                }
+            }
+            return Courses;
         }
 
-        public IActionResult Contact()
+        private List<string> getList(Dictionary<string, string> Subjects, KeyValuePair<string, string> main, List<string> result)
         {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if(result.Count > Subjects.Count*2)
+            {
+                return null;
+            }
+            foreach(KeyValuePair<string, string> sub in Subjects)
+            {
+                if(main.Value == sub.Key && sub.Value != "")
+                {
+                    result.Add(sub.Value);
+                    getList(Subjects, sub, result);
+                }
+            }
+            return result;
         }
     }
 }
--- Test/wwwroot/js/site.js
+++ Test/wwwroot/js/site.js
@@ -1,1 +1,16 @@
-﻿// Write your JavaScript code.
+﻿
+document.getElementById("submit").addEventListener("click", function () {
+    var data = {
+        input: $("#input").val()
+    };
+    $.ajax({
+        url: "/Home/Check",
+        type: "POST",
+        dataType: "json",
+        data: data,
+        success: function (mydata) {
+            if (!mydata) $("#output").val("Error occured");
+            else $("#output").val(mydata);
+        }
+    });
+});
